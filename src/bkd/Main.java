a8b5d80c485fbddd0e3b37bf3cc1f54fb4f84a8a
william <william@william-pc>
2020-11-15 17:42:25 -0500

    Update .classpath, .gitignore, and 2 more files...

--- src/bkd/Main.java
+++ src/bkd/Main.java
@@ -9,16 +9,16 @@ public class Main {
 	static FileReader files;
 	static Queue<String> list = new LinkedList<>();
 	static HashMap<String, LinkedList<String>> map = new HashMap<>();
-	static String dir = System.getProperty("user.dir") + "\\Saves";
+	static String dir = System.getProperty("user.dir") + "\\WebMapData";
 	public static void main(String[] args) throws IOException {
-		/*
+		
 		BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
 		args = new String[3];
 		args[0] = s.readLine();
 		args[1] = s.readLine();
 		args[2] = s.readLine();
 		s.close();
-		*/
+		
 		if(!new File(dir).isDirectory()) {
 			new File(dir).mkdir();
 		}
@@ -54,10 +54,10 @@ public class Main {
 					html = Jsoup.connect(turl).get();
 					turl = html.location();
 				} catch(Exception ex) {
-					System.out.println("Syntax Error: " + turl);
-					i--;
-					continue;
-					//return;
+					System.out.println("Unable to reach: " + turl);
+					//i--;
+					//continue;
+					return;
 				}
 				for(Element e:html.select("a[href]")) {
 					String nlink = urlmerge(e.attr("href"), turl);
@@ -96,26 +96,24 @@ public class Main {
 	static String urlmerge(String url, String lurl) {
 		if(url.length() == 0) return lurl;
 		String[] parts = lurl.split("/");
-		//System.out.println();
-		//System.out.println(lurl+ " "+url);
+		System.out.println();
+		System.out.println(lurl+ " "+url);
+		try {
+			if(url.startsWith("http:")||url.startsWith("https:")) return url;
+			if(url.contains(":")) return lurl;
+			if(url.startsWith("//")) return parts[0]+url;
+		}catch(Exception ignored) {}
 		if(url.charAt(0) == '#') {
 			return lurl+url;
 		}
-		try {
-			if(url.startsWith("javascript:")) return lurl;
-			if(url.startsWith("mail:")) return lurl;
-			if(url.startsWith("http:")||url.startsWith("https:")) return url;
-			if(url.startsWith("//")) {
-				return parts[0]+url;
-			}
-		}catch(Exception ignored) {}
 		try {
 		if(url.charAt(0) == '/') {
 			lurl = parts[0]+"//"+parts[2];
 		}
 		else if(url.charAt(0) == '.') {
-			if(parts.length>2 && parts[parts.length-1].contains(".")) {
-				lurl = lurl.substring(0, lurl.length()-(parts[parts.length-1].length()+1));
+			if(parts.length>3 && parts[parts.length-1].contains(".")) {
+				lurl = lurl.substring(0, lurl.length()-(parts[parts.length-1].length()));
+				parts = lurl.split("/");
 			}
 			if(url.startsWith("./")) {
 				url = url.substring(2, url.length());
@@ -131,7 +129,7 @@ public class Main {
 			else return lurl;
 		}
 		else {
-			if(parts.length>2 && parts[parts.length-1].contains(".")) {
+			if(parts.length>3 && parts[parts.length-1].contains(".")) {
 				lurl = lurl.substring(0, lurl.length()-(parts[parts.length-1].length()+1));
 			}
 		}
@@ -143,7 +141,7 @@ public class Main {
 			url = "/"+url;
 		}
 		url = lurl + url;
-		//System.out.println(url);
+		System.out.println(url);
 		return url;
 	}
 }
